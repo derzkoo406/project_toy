35a10f238cf74014db805e740a9e61ac20d48b05
muxr <derzkoo406@gmail.com>
2025-10-07 14:33:32 +0500

    kelin bilan kuyov

---
+++ lll.py
@@ -0,0 +1,16 @@
+# import requests
+#
+#
+# data=requests.get("https://cbu.uz/uz/arkhiv-kursov-valyut/json/")
+# jami_data=data.json()
+#
+# for money in jami_data:
+#     # print(money.get('Rate'))
+#     # for k,v in money.items():
+#     #     print(f"{k}------------>{v}")
+#
+#     birlik=money["Ccy"]
+#     qiymat=money("Rate")
+#     answer=f"{birlik} <-------> {qiymat}"
+#     print(answer)
+
---
+++ й.py
@@ -0,0 +1,18 @@
+
+# import requests
+#
+#
+# data=requests.get("https://cbu.uz/uz/arkhiv-kursov-valyut/json/").json()
+# birlik=['USD','EUR','RUB']
+# user=input('pul birligni tanlang').upper()
+# qancha=float(input('qancha pul maydalaysiz'))
+# if user not  in birlik:
+#     print('pul faqat usd,eur,rub')
+# for d in data:  # list
+#     if user==d['Ccy']:
+#         dollor=d['Rate']
+#         javob=qancha/float(dollor)
+#         print(javob,d['Date'],d['CcyNm_UZ'])
+
+
+
